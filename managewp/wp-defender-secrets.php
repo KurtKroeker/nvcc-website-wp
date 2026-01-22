ed3e05dc868597d51a5b1172c3fd80a030e3cdf7
Kurt Kroeker <kpk5008@gmail.com>
2026-01-21 22:05:02 -0500

    init

---
+++ managewp/wp-defender-secrets.php
@@ -0,0 +1,8 @@
+<?php
+// The file is used to generate and validate secret keys. Deleting this file or manually editing the content will permanently break 2fa TOTP method for all users.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+// WP Defender - Start
+define( 'WP_DEFENDER_TOTP_KEY', 'N87NVCH6O5MrgWKZL0JK9IbtzNijhez7+zAHzuTfAfQ=' );
+// WP Defender - End
